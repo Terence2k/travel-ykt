822a0c6b5429194d8cba5a7e020ed84df8cdc696
tanjh <tanjh@gzsendi.cn>
2022-09-20 17:55:58 +0800

    feat:景区查看页面api

--- src/views/scenicSpot/scenicSpotInformation/components/info.vue
+++ src/views/scenicSpot/scenicSpotInformation/components/info.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="editWrapper">
 		<header>基本信息</header>
-		<a-form class="" ref="formRef" :label-col="{ span: 3 }" labelAlign="left" :wrapper-col="{ span: 6 }" :scrollToFirstError="true">
+		<a-form class="" ref="formRef" :label-col="{ span: 4 }" labelAlign="left" :wrapper-col="{ span: 6 }" :scrollToFirstError="true">
 			<a-form-item label="企业名称" v-bind="validateInfos[`data.name`]">
 				<!-- <a-input v-model:value="formData.data.name" placeholder="请填写景区名字" /> -->
 				{{ formData.data.name }}
@@ -50,38 +50,40 @@
 			<div class="title">减免属性</div>
 			<a-form-item label="是否支持减免" v-bind="validateInfos[`data.derate`]">
 				{{ formData.data.derate ? '是' : '否' }}
-				<a-radio-group v-model:value="formData.data.derate">
-					<a-radio :value="true">是</a-radio>
-					<a-radio :value="false">否</a-radio>
-				</a-radio-group>
 			</a-form-item>
-			<a-form-item label="减免规则" v-bind="validateInfos[`data.derateRule`]">
-				<a-input
+			<a-form-item label="减免规则" v-bind="validateInfos[`data.derateRule`]" v-show="formData.data.derate">
+				{{ formData.data.derateRule }}
+				{{ '满' + formData.data.derateRule?.split(',')[0] + '减' + formData.data.derateRule?.split(',')[1] }}
+				<!-- <a-input
 					:disabled="formData.data.derate ? false : true"
 					v-model:value="formData.data.derateRule"
 					placeholder="减免规则详情，逗号隔开满16-9 ：16,9"
-				/>
+				/> -->
 			</a-form-item>
 			<div class="title">结算（收款）账户信息</div>
 			<a-form-item label="账户类型" v-bind="validateInfos[`data.accountType`]">
-				<a-select allowClear v-model:value="formData.data.accountType" :options="options" placeholder="请选择账户类型"></a-select>
+				{{ formData.data.accountType }}
+				<!-- <a-select allowClear v-model:value="formData.data.accountType" :options="options" placeholder="请选择账户类型"></a-select> -->
 			</a-form-item>
 			<a-form-item label="收款账号" v-bind="validateInfos[`data.bankAccount`]">
-				<a-input v-model:value="formData.data.bankAccount" placeholder="银行账号" />
+				{{ formData.data.bankAccount }}
+				<!-- <a-input v-model:value="formData.data.bankAccount" placeholder="银行账号" /> -->
 			</a-form-item>
 			<a-form-item label="账户名" v-bind="validateInfos[`data.bankAccountName`]">
-				<a-input v-model:value="formData.data.bankAccountName" />
+				{{ formData.data.bankAccountName }}
+				<!-- <a-input v-model:value="formData.data.bankAccountName" /> -->
 			</a-form-item>
 			<a-form-item label="开户行地址" v-bind="validateInfos[`data.accountAddress`]">
-				<a-input v-model:value="formData.data.accountAddress" />
+				{{ formData.data.accountAddress }}
+				<!-- <a-input v-model:value="formData.data.accountAddress" /> -->
 			</a-form-item>
 			<a-form-item label="还款行" v-bind="validateInfos[`data.bank`]">
-				<a-input v-model:value="formData.data.bank" />
+				{{ formData.data.bank }}
+				<!-- <a-input v-model:value="formData.data.bank" /> -->
 			</a-form-item>
 
 			<footer>
-				<a-button type="primary" htmlType="{submit}" @click.prevent="onSubmit">保存</a-button>
-				<a-button style="margin-left: 10px" @click="reset">提交审核</a-button>
+				<a-button type="primary" htmlType="{submit}" @click.prevent="onSubmit">编辑</a-button>
 			</footer>
 			<!-- <a-form-item :wrapper-col="{ span: 14, offset: 4 }"> </a-form-item> -->
 		</a-form>
@@ -105,10 +107,6 @@ const popupScroll = () => {
 const options = [];
 // 数据
 const formData = reactive({
-	name: '',
-	sub: {
-		name: '',
-	},
 	data: {
 		oid: 1, //oid
 
@@ -166,61 +164,19 @@ const formData = reactive({
 	},
 });
 // 表单
-const { resetFields, validate, validateInfos, mergeValidateInfo, scrollToField } = useForm(
-	formData,
-	reactive({
-		'data.provinceId': [{ required: true, message: '请选择省份' }],
-		'data.cityId': [{ required: true, message: '请选择市' }],
-		'data.areaId': [{ required: true, message: '请选择县区' }],
-		'data.addressDetail': [{ required: true, message: '请填写详细地址' }],
-		'data.name': [{ required: true, message: '请输入景区名字' }],
-		'data.scenicLevel': [{ required: true, message: '请选择景区等级' }],
-		'data.creditCode': [{ required: true, message: '请填写统一社会行用代码' }],
-		'data.phone': [{ required: true, message: '请填写手机号码' }],
-		'data.contactName': [{ required: true, message: '请填写联系人姓名' }],
-		'data.businessLicenseUrl': [{ required: true, message: '请上传图片' }],
-		'data.derate': [{ required: true, message: '请选择' }],
-		'data.derateRule': [{ message: '请填写规则' }],
-		'data.accountType': [{ required: true, message: '请选择账户类型' }],
-		'data.bankAccount': [{ required: true, message: '请填写银行账号' }],
-		'data.accountAddress': [{ required: true, message: '请填写开户地址' }],
-		'data.businessType': [{ required: true, message: '请选择企业业态' }],
-		'data.bankAccountName': [{ required: true, message: '请填写银行账户名' }],
-		'data.unitStatus': [{ required: true, message: '请选择企业状态' }],
-	})
-);
-const formRef = ref();
+const { resetFields, validate, validateInfos, mergeValidateInfo, scrollToField } = useForm(formData);
 // 提交
 const onSubmit = async () => {
-	validate()
-		.then((res) => {
-			console.log(res, toRaw(formData), 'psss');
-		})
-		.catch((err) => {
-			console.log('error', err);
-		});
+	route.push({ path: '/scenic-spot/information/edit', query: { oid: encodeURIComponent(formData.data.oid) } });
+};
 
-	// try {
-	// 	const values = await validate();
-	// 	console.log('Success:', values);
-	// } catch (errorInfo) {
-	// 	//返回报错信息，滚动到第一个报错的位置
-	// 	formRef.scrollToField(errorInfo.errorFields[0].name.toString());
-	// }
-};
-//合并错误提示
-const errorInfos = computed(() => {
-	return mergeValidateInfo(toArray(validateInfos).splice(0, 4));
-});
-// 重置
-const reset = (): void => {
-	resetFields();
-};
 //初始化页面
 const initPage = async (): Promise<void> => {
 	formData.data.oid = route.currentRoute.value?.query?.oid;
 	let res = await api.getScenicById(formData.data.oid);
-	console.log(res);
+	console.log(res, 'res');
+	formData.data = res;
+	formData.data.oid = route.currentRoute.value?.query?.oid;
 };
 
 // 自定义面包屑
