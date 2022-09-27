472659b05e8af34134121256db47e5441d7c4a99
linjj <386586437@qq.com>
2022-09-27 18:02:59 +0800

    🐞 fix: 修改产品结算规则检验

--- src/views/settlementManagement/productSettlementRule/components/type.ts
+++ src/views/settlementManagement/productSettlementRule/components/type.ts
@@ -1,11 +1,11 @@
 export interface FormState {
 	costName?: string | null;
 	costExplanation?: string | null;
-	splitList?: Array<splitListType>;
+	splitList: Array<splitListType>;
 	teamTypeId?: number | null;
 	ruleStatus?: number | null;
 	productId?: number | null;
-	prepaidCompany?: Array<any>;
+	prepaidCompany?: number | null;
 	chargeProductSonId?: number | null;
 	chargeCount?: number | null;
 	isPrepaid?: number | null;
