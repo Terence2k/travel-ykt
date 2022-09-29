0393649b3d4a8219ec2034c1c1807c678bd06d6a
wuzew <971717643@qq.com>
2022-09-29 11:24:56 +0800

    修复表单自定义验证规则控制台报错

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,30 +1,37 @@
 // 验证是否整数
 export function isIntegerNotMust(rule: any, value: any, callback: any) {
 	if (!Number(value)) {
-		callback(new Error('请输入正整数'));
+		// callback(new Error('请输入正整数'));
+		return Promise.reject(new Error('请输入正整数'));
 	} else {
 		const re = /^[0-9]*[1-9][0-9]*$/;
 		const rsCheck = re.test(value);
 		if (!rsCheck) {
-			callback(new Error('请输入正整数'));
+			// callback(new Error('请输入正整数'));
+			return Promise.reject(new Error('请输入正整数'));
 		} else {
-			callback();
+			// callback();
+			return Promise.resolve();
 		}
 	}
 }
 // 验证是否是[0-100]的小数
 export function isBtnZeroToHundred(rule: any, value: any, callback: any) {
 	if (!value) {
-		return callback(new Error('输入不可以为空'));
+		// return callback(new Error('输入不可以为空'));
+		return Promise.reject(new Error('输入不可以为空'));
 	}
 	setTimeout(() => {
 		if (!Number(value)) {
-			callback(new Error('请输入[1,100]之间的数字'));
+			// callback(new Error('请输入[1,100]之间的数字'));
+			return Promise.reject(new Error('请输入[1,100]之间的数字'));
 		} else {
 			if (value < 0 || value > 100) {
-				callback(new Error('请输入[1,100]之间的数字'));
+				// callback(new Error('请输入[1,100]之间的数字'));
+				return Promise.reject(new Error('请输入[1,100]之间的数字'));
 			} else {
-				callback();
+				// callback();
+				return Promise.resolve();
 			}
 		}
 	}, 100);
