45c81cb492eedfb6493d365d1185be7066c109b7
tanjh <tanjh@gzsendi.cn>
2022-10-24 16:32:53 +0800

    feat: 价格日历

--- src/views/scenicSpot/multicastTick/components/tablePrice.vue
+++ src/views/scenicSpot/multicastTick/components/tablePrice.vue
@@ -10,51 +10,61 @@
 			</template>
 		</CommonTable>
 
-		<BaseModal :modelValue="modelValue" title="设置减免规则" @cancel="cancel" width="1210px">
-			<p>valueNext {{ valueNext }}</p>
-			<p>nextDay{{ nextDay }}</p>
-			<p>value{{ value }}</p>
-			<p>preDay{{ preDay }}</p>
+		<Calendar
+			ref="calendarRef"
+			:setAllValue="allPrice"
+			:setCurrentValue="currentPrict"
+			:setList="setDayPriceList"
+			@get-current-day="getCurrentDay"
+			@clear-current-day="clearCurrentDay"
+		>
 			<header class="tips">
 				<p>说明：点击后编辑每日库存，不编辑默认库存为默认</p>
 				<p>说明：点击后编辑每日价格，不编辑则默认价格为默认价格，价格为默认价格时不可保存价格日历</p>
 			</header>
 
-			<article class="calendar-wrap">
-				<div :style="{ width: '800px', border: '1px solid #d9d9d9', borderRadius: '4px' }">
-					<a-calendar v-model:value="value" :fullscreen="false" @panelChange="onPanelChange">
-						<template v-slot:dateCellRender="{ current }">
-							<span :class="isCurrentDay(current) ? 'price_tips default' : 'price_tips'">￥{{ isCurrentDay(current) }}</span>
-						</template>
-					</a-calendar>
+			<section>
+				<div class="set-wrap">
+					<p>
+						批量设置
+						<a-input-number
+							:min="0"
+							:max="9999999999"
+							v-model:value="allPrice"
+							:formatter="(value) => value.replace(/\D/g, '')"
+							:parser="(value) => value.replace(/\D/g, '')"
+							placeholder="输入统一票价"
+							style="width: 200px"
+						/>
+					</p>
+					<p>
+						按日设置
+						<a-input-number
+							:min="0"
+							:max="9999999999"
+							v-model:value="currentPrict"
+							:formatter="(value) => value.replace(/\D/g, '')"
+							:parser="(value) => value.replace(/\D/g, '')"
+							placeholder="输入当日票价"
+							style="width: 200px"
+						/>
+						<a-button @click="createDateItem">确定</a-button>
+					</p>
 				</div>
-				<div :style="{ width: '800px', border: '1px solid #d9d9d9', borderRadius: '4px' }">
-					<a-calendar v-model:value="valueNext" :fullscreen="false" @panelChange="onPanelChange">
-						<template v-slot:dateCellRender="{ current }">
-							<span class="price_tips">￥{{ shijianYMD(current) }}</span>
-						</template>
-					</a-calendar>
-				</div>
-			</article>
-
-			<template v-slot:footer>
-				<a-button type="primary" @click="apply" style="width: 100px">保存</a-button>
-				<a-button @click="cancel">取消</a-button>
-			</template>
-		</BaseModal>
-		<DelModal :params="{ title: '删除', content: '是否确定该条数据' }" v-model="delShow" @submit="delSubmit" @cancel="delCancel" />
+			</section>
+		</Calendar>
 	</div>
 </template>
 
 <script setup lang="ts">
 import CommonTable from '@/components/common/CommonTable.vue';
-import BaseModal from '@/components/common/BaseModal.vue';
-import DelModal from '@/components/common/DelModal.vue';
 import dayjs, { Dayjs } from 'dayjs';
 import { Form } from 'ant-design-vue';
+import Calendar from '@/components/common/calendarDouble.vue';
 
 import api from '@/api';
 import { message } from 'ant-design-vue';
+import { log } from 'console';
 
 // 数据
 const props = defineProps({
@@ -63,68 +73,7 @@ const props = defineProps({
 		default: () => [],
 		require: true,
 	},
-	// params: Object,
-	// tableList: Array,
 });
-const value = ref<Dayjs>(dayjs('2022-10-6 9:00:00'));
-const valueNext = ref<Dayjs>(dayjs('2022-11-6 9:00:00'));
-const onPanelChange = (value: Dayjs, mode: string) => {
-	console.log(value, mode);
-};
-
-const arr = ['2022-10-20', '2022-10-21'];
-
-const preDay = computed(() => {
-	let time = new Date(value.value);
-
-	let year = Number(time.getFullYear());
-	let month = Number((time.getMonth() + 1).toString().padStart(2, '0'));
-	const date = time.getDate().toString().padStart(2, '0');
-	month - 1 === 0 ? year-- : (month -= 1);
-	let day = year + '-' + month + '-' + date;
-	return dayjs(day);
-});
-const nextDay = computed(() => {
-	let time = new Date(value.value);
-
-	let year = time.getFullYear();
-	let month = Number((time.getMonth() + 1).toString().padStart(2, '0'));
-	const date = time.getDate().toString().padStart(2, '0');
-	month + 1 === 13 ? year++ : (month += 1);
-	let day = year + '-' + month + '-' + date;
-	return dayjs(day);
-});
-const styleColor = ref(false);
-const shijianYMD = (timestamp: any) => {
-	let time = new Date(timestamp);
-
-	let year = time.getFullYear();
-	const month = (time.getMonth() + 1).toString().padStart(2, '0');
-	const date = time.getDate().toString().padStart(2, '0');
-
-	return year + '-' + month + '-' + date;
-};
-
-const isCurrentDay = (timestamp: any) => {
-	let day = shijianYMD(timestamp);
-
-	if (arr.includes(day)) {
-		return true;
-	} else {
-		return false;
-	}
-};
-
-const route = useRouter();
-
-const useForm = Form.useForm;
-// 新增减免规则
-const formValidate = reactive({
-	certifId: [],
-	discount: null,
-	ruleName: '',
-});
-
 const columnsCount = ref([
 	{
 		title: '子票',
@@ -145,26 +94,74 @@ const columnsCount = ref([
 		width: 200,
 	},
 ]);
-// 删除提示
-const delShow = ref(false);
-const delIndex = ref<null | number>();
-const emits = defineEmits(['del-rule-obj', 'add-rule-obj']);
-const del = (index: number) => {
-	// emits('del-rule-obj', index);
-	delShow.value = true;
-	delIndex.value = index;
+
+//自定义价格列表
+const setDayPriceList = ref([
+	{ day: '2022-10-20', price: '30' },
+	{ day: '2022-10-21', price: '13' },
+]);
+
+//日历
+const currentPrict = ref(null);
+const allPrice = ref(40);
+
+//getCurrentDay
+const currentDay = ref();
+
+const getCurrentDay = (day: string) => {
+	currentDay.value = day;
 };
-const delSubmit = () => {
-	emits('del-rule-obj', toRaw(delIndex.value));
-	delCancel();
+
+const clearCurrentDay = () => {
+	currentDay.value = null;
+	currentPrict.value = null;
+	calendarRef.value.clear();
 };
-const delCancel = () => {
-	delShow.value = false;
-	delIndex.value = null;
+
+const createDateItem = () => {
+	if (!currentDay.value) {
+		message.error('请选择日期');
+		return;
+	}
+
+	if (typeof currentPrict.value !== 'number') {
+		message.error('请填写价格');
+		return;
+	}
+	console.log('createDateItem', currentPrict.value, currentDay.value);
+
+	let obj = { day: currentDay.value, price: currentPrict.value },
+		isEdit = false;
+
+	setDayPriceList.value.map((i, index) => {
+		if (i.day === currentDay.value) {
+			isEdit = true;
+			editItem(index, obj);
+		}
+		return i;
+	});
+
+	if (!isEdit) {
+		createItem(obj);
+	}
+
+	message.success('成功');
+	clearCurrentDay();
 };
+
+const createItem = (obj: any) => {
+	setDayPriceList.value.push(obj);
+};
+
+const editItem = (index: number, obj: any) => {
+	setDayPriceList.value[index] = obj;
+};
+//弹窗部分
 const modelValue = ref(false);
+const calendarRef = ref();
 const CreateData = () => {
-	modelValue.value = true;
+	// modelValue.value = true;
+	calendarRef.value.open();
 };
 
 const cancel = () => {
@@ -179,21 +176,12 @@ const apply = () => {
 			resetFields();
 			console.log(formValidate, res);
 
-			emits('add-rule-obj', toRaw(res));
+			// emits('add-rule-obj', toRaw(res));
 		})
 		.catch((err) => {
 			console.log('error', err);
 		});
 };
-// 表单
-const { resetFields, validate, validateInfos, mergeValidateInfo, scrollToField } = useForm(
-	formValidate,
-	reactive({
-		certifId: [{ required: true, message: '请选择类型' }],
-		discount: [{ required: true, message: '请输入0-10', pattern: /^([0-9]|10)$/ }],
-		ruleName: [{ required: true, message: '请填写' }],
-	})
-);
 
 onMounted(() => {});
 </script>
@@ -205,15 +193,88 @@ onMounted(() => {});
 
 .calendar-wrap {
 	display: flex;
-	.price_tips {
-		display: block;
-		color: #ff9f3f;
+	height: 100%;
+	position: relative;
+	padding: 20px;
+	.turn-left {
+		position: absolute;
+		left: 0;
+		top: 50%;
+		width: 20px;
+		height: 20px;
+		text-align: center;
+		// background-color: red;
+		cursor: pointer;
 	}
-	.price_tips.default {
-		color: #ddd;
+	.turn-right {
+		position: absolute;
+		right: 0;
+		top: 50%;
+		width: 20px;
+		height: 20px;
+		text-align: center;
+		// background-color: red;
+		cursor: pointer;
+	}
+	.wrap {
+		position: relative;
+		width: 568px;
+		border: 1px solid #f1f2f5;
+		.price_tips {
+			color: #ff9f3f;
+		}
+		.price_tips.default {
+			color: #ddd;
+		}
+		.calendar-header {
+			text-align: center;
+			height: 40px;
+			line-height: 40px;
+			font-size: 16px;
+			font-weight: bold;
+			border: none;
+		}
+		.date-wrap {
+			padding-top: 6px;
+			width: 82px;
+			height: 50px;
+			line-height: 18px;
+			font-size: 14px;
+			text-align: center;
+		}
 	}
 }
 .table-area {
 	padding: 0;
+}
+
+::v-deep .ant-picker-calendar .ant-picker-panel {
+	border-top: none;
+}
+::v-deep thead {
+	padding: 10px;
+	height: 44px;
+	border-bottom: 1px solid #f1f2f5 !important;
+}
+::v-deep thead tr th::before {
+	content: '周';
+	font-size: 14px;
+	font-weight: 400;
+}
+::v-deep thead tr th {
+	font-size: 14px;
+	font-weight: 400;
+}
+::v-deep tbody tr:first-child {
+	padding-top: 10px;
+	margin-top: 10px;
+}
+
+::v-deep .ant-picker-cell.ant-picker-cell-in-view {
+	padding: 0;
+}
+::v-deep .ant-picker-body {
+	padding: 0 !important;
+	margin: 0;
 }
 </style>
