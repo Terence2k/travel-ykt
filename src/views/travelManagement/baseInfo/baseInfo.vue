8458465baed6f2c47767070c601c59e920dd9ab6
luoyq <luoyq@gzsendi.cn>
2022-10-24 16:53:27 +0800

    无

--- src/views/travelManagement/baseInfo/baseInfo.vue
+++ src/views/travelManagement/baseInfo/baseInfo.vue
@@ -272,7 +272,6 @@ watch(() => props.onCheck, (newVal) => {
 	onSubmit()
 })
 watch(() => travelStore.baseInfo, newVal => {
-	console.log(newVal,'11111111111111111111111')
 	formState.value = newVal;
 	if (route.query.id) {
 		list.travelOperatorList = [newVal.subTravelOperator];
