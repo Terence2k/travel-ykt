7f34976edb636a66d0d5f322d97330cf36431bfc
wuzew <971717643@qq.com>
2022-10-24 17:04:06 +0800

    增加报表管理部分列表

--- src/router/modules/reportManagement.route.ts
+++ src/router/modules/reportManagement.route.ts
@@ -13,7 +13,7 @@ export default [
 				name: 'scenicSpotTransferReport',
 				redirect: '/reportManagement/scenicSpotTransferReport/list',
 				meta: {
-					title: '景区行程单报表',
+					title: '景区行程单结算表',
 					isDetail: false, // 在左侧的导航栏不会展示
 				},
 				children: [
@@ -42,7 +42,7 @@ export default [
 				name: 'scenicSpotOrderReport',
 				redirect: '/reportManagement/scenicSpotOrderReport/list',
 				meta: {
-					title: '景区订单结算报表',
+					title: '景区订单结算表',
 					isDetail: false, // 在左侧的导航栏不会展示
 				},
 				children: [
@@ -98,7 +98,123 @@ export default [
 					{
 						path: 'orderList',
 						component: () => import('@/views/reportManagement/scenicSpotTransferReport/components/tripList.vue'),
-						name: 'scenicSpotTransferReportTripList',
+						name: 'hotelItineraryReportOrderList',
+						meta: {
+							title: '查看订单',
+							isDetail: true, // 在左侧的导航栏不会展示
+						},
+					},
+				],
+			},
+			{
+				path: 'groupItineraryReport',
+				name: 'groupItineraryReport',
+				redirect: '/reportManagement/groupItineraryReport/list',
+				meta: {
+					title: '集团行程单结算表',
+					isDetail: true, // 在左侧的导航栏不会展示
+				},
+				children: [
+					{
+						path: 'list',
+						component: () => import('@/views/reportManagement/scenicSpotTransferReport/index.vue'),
+						name: 'groupItineraryReportList',
+						meta: {
+							title: '首页',
+							isDetail: true, // 在左侧的导航栏不会展示
+						},
+					},
+					{
+						path: 'orderList',
+						component: () => import('@/views/reportManagement/scenicSpotTransferReport/components/tripList.vue'),
+						name: 'groupItineraryReportOrderList',
+						meta: {
+							title: '查看订单',
+							isDetail: true, // 在左侧的导航栏不会展示
+						},
+					},
+				],
+			},
+			{
+				path: 'associationItineraryReport',
+				name: 'associationItineraryReport',
+				redirect: '/reportManagement/associationItineraryReport/list',
+				meta: {
+					title: '协会行程单结算表',
+					isDetail: true, // 在左侧的导航栏不会展示
+				},
+				children: [
+					{
+						path: 'list',
+						component: () => import('@/views/reportManagement/scenicSpotTransferReport/index.vue'),
+						name: 'associationItineraryReportList',
+						meta: {
+							title: '首页',
+							isDetail: true, // 在左侧的导航栏不会展示
+						},
+					},
+					{
+						path: 'orderList',
+						component: () => import('@/views/reportManagement/scenicSpotTransferReport/components/tripList.vue'),
+						name: 'associationItineraryReportOrderList',
+						meta: {
+							title: '查看订单',
+							isDetail: true, // 在左侧的导航栏不会展示
+						},
+					},
+				],
+			},
+			{
+				path: 'supervisorItineraryReport',
+				name: 'supervisorItineraryReport',
+				redirect: '/reportManagement/supervisorItineraryReport/list',
+				meta: {
+					title: '监理行程单结算表',
+					isDetail: true, // 在左侧的导航栏不会展示
+				},
+				children: [
+					{
+						path: 'list',
+						component: () => import('@/views/reportManagement/scenicSpotTransferReport/index.vue'),
+						name: 'supervisorItineraryReportList',
+						meta: {
+							title: '首页',
+							isDetail: true, // 在左侧的导航栏不会展示
+						},
+					},
+					{
+						path: 'orderList',
+						component: () => import('@/views/reportManagement/scenicSpotTransferReport/components/tripList.vue'),
+						name: 'supervisorItineraryReportOrderList',
+						meta: {
+							title: '查看订单',
+							isDetail: true, // 在左侧的导航栏不会展示
+						},
+					},
+				],
+			},
+			{
+				path: 'yktItineraryReport',
+				name: 'yktItineraryReport',
+				redirect: '/reportManagement/yktItineraryReport/list',
+				meta: {
+					title: '一卡通行程单结算表',
+					isDetail: true, // 在左侧的导航栏不会展示
+				},
+				children: [
+					{
+						path: 'list',
+						component: () => import('@/views/reportManagement/scenicSpotTransferReport/index.vue'),
+						name: 'yktItineraryReportList',
+						meta: {
+							title: '首页',
+							isDetail: true, // 在左侧的导航栏不会展示
+						},
+					},
+					{
+						path: 'orderList',
+						component: () => import('@/views/reportManagement/scenicSpotTransferReport/components/tripList.vue'),
+						name: 'yktItineraryReportOrderList',
 						meta: {
 							title: '查看订单',
 							isDetail: true, // 在左侧的导航栏不会展示
@@ -107,5 +223,6 @@ export default [
 				],
 			},
 		],
+		
 	},
 ];
