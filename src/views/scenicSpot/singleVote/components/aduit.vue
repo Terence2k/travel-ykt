c68f42f3f8daad15592d12c4565360a321806946
tanjh <tanjh@gzsendi.cn>
2022-11-03 14:21:37 +0800

    feat： 景区上下架精确到分

--- src/views/scenicSpot/singleVote/components/aduit.vue
+++ src/views/scenicSpot/singleVote/components/aduit.vue
@@ -11,10 +11,10 @@
 									class="data-item"
 									v-model:value="formValidate.data.dateList[index].time"
 									format="YYYY-MM-DD HH:mm"
-									valueFormat="YYYY-MM-DD HH:mm:ss"
+									valueFormat="YYYY-MM-DD HH:mm"
 									:show-time="{
 										hideDisabledOptions: true,
-										defaultValue: [dayjs(' 00:00:00', 'HH:mm'), dayjs('23:59:59', 'HH:mm:ss')],
+										defaultValue: [dayjs(' 00:00:00', 'HH:mm'), dayjs('23:59:59', 'HH:mm')],
 									}"
 								/>
 							</a-form-item>
