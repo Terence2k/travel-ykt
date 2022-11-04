40b93b04e1ff52471078bc590fcdde3ada183eb1
卢智锋 <luzf@baiyingtech.cn>
2022-11-04 17:32:57 +0800

    feat:添加用户列表权限控制

--- src/views/baseInfoManage/userManage/index.vue
+++ src/views/baseInfoManage/userManage/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <CommonSearch v-if="isBusinessSuperAdmin">
+  <CommonSearch v-if="!isBusinessSuperAdmin">
     <search-item label="角色名称">
       <a-input v-model:value="state.tableData.param.roleName" placeholder="请输入角色名称"/>
     </search-item>
@@ -23,7 +23,7 @@
   </CommonSearch>
   <CommonTable :dataSource="state.tableData.data" :columns="columns">
       <template #button>
-        <a-button type="primary" @click="addOrUpdate({ handle: 'add' })">新增</a-button>
+        <a-button type="primary" @click="addOrUpdate({ handle: 'add' })" v-if="!isGroupSuperAdmin">新增</a-button>
       </template>
       <template #bodyCell="{ column, index, record }">
         <template v-if="column.key === 'index'">
@@ -39,9 +39,12 @@
         <template v-if="column.key === 'action'">
           <div class="action-btns">
             <a @click="addOrUpdate({  row: record,  handle: 'update'})">编辑</a>
-            <a @click="resetPassword(record.oid)">重置密码</a>
-            <a @click="editStatus(record.oid, 0)" v-if="record.userStatus === 1">禁用</a>
-            <a @click="editStatus(record.oid, 1)" v-if="record.userStatus === 0">启用</a>
+            <!-- <a @click="resetPassword(record.oid)">重置密码</a> -->
+            <a @click="addOrUpdate({  row: record,  handle: 'update'})">重置密码</a>
+            <template v-if="record.roleList.some((item: any) => item.roleCode != 'PLATFORM_SUPER_ADMIN')">
+              <a @click="editStatus(record.oid, 0)" v-if="record.userStatus === 1">禁用</a>
+              <a @click="editStatus(record.oid, 1)" v-if="record.userStatus === 0">启用</a>
+            </template>
           </div>
         </template>
     </template>
@@ -70,54 +73,9 @@
   import { message } from 'ant-design-vue';
   import { getUserInfo } from '@/utils/util';
   import { Modal } from 'ant-design-vue';
+  import { Ref } from 'vue';
   
-  const columns = [
-    {
-      title: '序号',
-      key: 'index',
-      width: '80px'
-    },
-    {
-      title: '管理员姓名',
-      dataIndex: 'username',
-      key: 'username',
-    },
-    {
-      title: '管理员手机号',
-      dataIndex: 'mobile',
-      key: 'mobile',
-    },
-    {
-      title: '登录账号',
-      dataIndex: 'account',
-      key: 'account',
-    },
-    {
-      title: '管理员角色',
-      dataIndex: 'roleList',
-      key: 'roleList',
-    },
-    {
-      title: '账号创建时间',
-      dataIndex: 'createTime',
-      key: 'createTime',
-    },
-    {
-      title: '最后登录时间',
-      dataIndex: 'lastLoginTime',
-      key: 'lastLoginTime',
-    },
-    {
-      title: '当前状态',
-      dataIndex: 'userStatusName',
-      key: 'userStatusName',
-    },
-    {
-      title: '操作',
-      key: 'action',
-      width: 208
-    },
-  ]
+  const columns: Ref<Array<any>> = ref([]);
 
   const state = reactive({
     tableData: {
@@ -143,6 +101,7 @@
   });
   const userInfo = getUserInfo();
   const isBusinessSuperAdmin = ref(false);
+  const isGroupSuperAdmin = ref(false);
 
   const onHandleCurrentChange = (val: number) => {
     console.log('change:', val);
@@ -191,31 +150,102 @@
     })
   }
 
-  const resetPassword = (id: any) => {
-      Modal.confirm({
-        title: '重置密码',
-        width: 560,
-        closable: true,
-        centered: true,
-        icon: false,
-        content: `是否确认重置密码？`,
-        onOk() {
-          let formData = new FormData();
-          formData.append('userId', id);
-          formData.append('newPassword', '123456');
-          api.adminResetPassword(formData).then((res: any) => {
-            message.success('密码重置成功');
-            onSearch();
-          })
-        },
-        onCancel() {},
-      });
-  }
+  // 重置密码
+  // const resetPassword = (id: any) => {
+  //     Modal.confirm({
+  //       title: '重置密码',
+  //       width: 560,
+  //       closable: true,
+  //       centered: true,
+  //       icon: false,
+  //       content: `是否确认重置密码？`,
+  //       onOk() {
+  //         let formData = new FormData();
+  //         formData.append('userId', id);
+  //         formData.append('newPassword', '123456');
+  //         api.adminResetPassword(formData).then((res: any) => {
+  //           message.success('密码重置成功');
+  //           onSearch();
+  //         })
+  //       },
+  //       onCancel() {},
+  //     });
+  // }
+
+  onBeforeMount(() => {
+    // 企业超级管理员不显示搜索区域
+    isBusinessSuperAdmin.value = userInfo.sysRoles.some((item: any) => !['PLATFORM_SUPER_ADMIN', 'GROUP_SUPER_ADMIN'].includes(item.roleCode));
+    // 集团超级管理员不显示操作
+    isGroupSuperAdmin.value = userInfo.sysRoles.some((item: any) => ['GROUP_SUPER_ADMIN'].includes(item.roleCode));
+    columns.value = [
+      {
+        title: '序号',
+        key: 'index',
+        width: '80px'
+      },
+      {
+        title: '所属企业',
+        dataIndex: 'unitName',
+        key: 'unitName',
+      },
+      {
+        title: '企业类型',
+        dataIndex: 'businessTypeName',
+        key: 'businessTypeName',
+      },
+      {
+        title: '管理员姓名',
+        dataIndex: 'username',
+        key: 'username',
+      },
+      {
+        title: '管理员手机号',
+        dataIndex: 'mobile',
+        key: 'mobile',
+      },
+      {
+        title: '登录账号',
+        dataIndex: 'account',
+        key: 'account',
+      },
+      {
+        title: '管理员角色',
+        dataIndex: 'roleList',
+        key: 'roleList',
+      },
+      {
+        title: '账号创建时间',
+        dataIndex: 'createTime',
+        key: 'createTime',
+      },
+      {
+        title: '最后登录时间',
+        dataIndex: 'lastLoginTime',
+        key: 'lastLoginTime',
+      },
+      {
+        title: '当前状态',
+        dataIndex: 'userStatusName',
+        key: 'userStatusName',
+      },
+      {
+        title: '操作',
+        key: 'action',
+        width: 208
+      },
+    ]
+    columns.value.forEach((item: any, index: any) => {
+      if (item.key === 'action' && isGroupSuperAdmin.value) {
+        delete columns.value[index];
+      }
+      if (['unitName', 'businessTypeName'].includes(item.key) && isBusinessSuperAdmin.value) {
+        delete columns.value[index];
+      }
+    })
+  })
 
   onMounted(() => {
     onSearch();
-    // 企业超级管理员不显示搜索区域
-    isBusinessSuperAdmin.value = userInfo.sysRoles.some((item: any) => ['PLATFORM_SUPER_ADMIN', 'GROUP_SUPER_ADMIN'].includes(item.roleCode));
   })
 </script>
 
