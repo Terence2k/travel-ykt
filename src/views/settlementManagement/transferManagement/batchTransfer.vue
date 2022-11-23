42be8c243d73cee4141bd2d65311b1d01c343ad8
林佳杰 <linjj@baiyingtech.cn>
2022-11-23 13:48:02 +0800

    🐞 fix:转账管理批量申请数据整理

--- src/views/settlementManagement/transferManagement/batchTransfer.vue
+++ src/views/settlementManagement/transferManagement/batchTransfer.vue
@@ -3,8 +3,8 @@
 		<div class="batchTransfer-header">
 			<div class="last-btn">
 				<div class="btn">
-					<a-button type="primary" class="success" @click="examine(1)">审核通过</a-button>
 					<a-button type="primary" class="success" @click="examine(0)">审核不通过</a-button>
+					<a-button type="primary" class="success" @click="examine(1)">审核通过</a-button>
 				</div>
 			</div>
 			<div class="header-total">
@@ -29,34 +29,220 @@ const state = reactive({
 const columns = computed(() => {
 	const column = [
 		{
-			title: '转账单号',
-			dataIndex: 'transferAccountsNo',
-			key: 'transferAccountsNo',
+			title: '组团社',
+			dataIndex: 'travelName',
+			key: 'travelName',
+			width: 100,
 		},
 		{
-			title: '转账单位',
-			dataIndex: 'transferAccountsCompanyName',
-			key: 'transferAccountsCompanyName',
+			title: '地接社',
+			dataIndex: 'subTravelName',
+			key: 'subTravelName',
+			width: 100,
 		},
 		{
-			title: '转账账户',
-			dataIndex: 'transferAccountsAccount',
-			key: 'transferAccountsAccount',
+			title: '团队类型',
+			dataIndex: 'travelTypeName',
+			key: 'travelTypeName',
+			width: 100,
 		},
 		{
-			title: '转账金额（元）',
-			dataIndex: 'transferAccountsMoney',
-			key: 'transferAccountsMoney',
+			title: '人数',
+			dataIndex: 'peopleNum',
+			key: 'peopleNum',
+			width: 80,
 		},
 		{
-			title: '收款单位',
-			dataIndex: 'collectionCompanyName',
-			key: 'collectionCompanyName',
+			title: '团款',
+			dataIndex: 'frozenPrice',
+			key: 'frozenPrice',
+			width: 100,
 		},
 		{
-			title: '收款账户',
-			dataIndex: 'collectionAccount',
-			key: 'collectionAccount',
+			title: '核销总费用',
+			dataIndex: 'settlementPrice',
+			key: 'settlementPrice',
+			width: 100,
+		},
+		{
+			title: '未消费费用',
+			dataIndex: 'unSettlementPrice',
+			key: 'unSettlementPrice',
+			width: 100,
+		},
+		{
+			title: '古维费用',
+			key: 'hmVo',
+			children: [
+				{
+					title: '冻结金额',
+					dataIndex: 'frozenPrice',
+					key: 'hmVo',
+					width: 100,
+				},
+				{
+					title: '已核销金额',
+					dataIndex: 'settlementPrice',
+					key: 'hmVo',
+					width: 100,
+				},
+				{
+					title: '实收',
+					dataIndex: 'actualPrice',
+					key: 'hmVo',
+					width: 100,
+				},
+			],
+		},
+		{
+			title: '景区',
+			key: 'ticketVo',
+			children: [
+				{
+					title: '冻结金额',
+					dataIndex: 'frozenPrice',
+					key: 'ticketVo',
+					width: 100,
+				},
+				{
+					title: '已核销金额',
+					dataIndex: 'settlementPrice',
+					key: 'ticketVo',
+					width: 100,
+				},
+				{
+					title: '实收',
+					dataIndex: 'actualPrice',
+					key: 'ticketVo',
+					width: 100,
+				},
+			],
+		},
+		{
+			title: '酒店',
+			key: 'hotelVo',
+			children: [
+				{
+					title: '冻结金额',
+					dataIndex: 'frozenPrice',
+					key: 'hotelVo',
+					width: 100,
+				},
+				{
+					title: '已核销金额',
+					dataIndex: 'settlementPrice',
+					key: 'hotelVo',
+					width: 100,
+				},
+				{
+					title: '实收',
+					dataIndex: 'actualPrice',
+					key: 'hotelVo',
+					width: 100,
+				},
+			],
+		},
+		{
+			title: '餐饮',
+			key: 'cateringVo',
+			children: [
+				{
+					title: '冻结金额',
+					dataIndex: 'frozenPrice',
+					key: 'cateringVo',
+					width: 100,
+				},
+				{
+					title: '已核销金额',
+					dataIndex: 'settlementPrice',
+					key: 'cateringVo',
+					width: 100,
+				},
+				{
+					title: '实收',
+					dataIndex: 'actualPrice',
+					key: 'cateringVo',
+					width: 100,
+				},
+			],
+		},
+		{
+			title: '监理',
+			key: 'superviseVo',
+			children: [
+				{
+					title: '实收',
+					dataIndex: 'actualPrice',
+					key: 'superviseVo',
+					width: 100,
+				},
+			],
+		},
+		{
+			title: '协会',
+			key: 'associationVo',
+			children: [
+				{
+					title: '实收',
+					dataIndex: 'actualPrice',
+					key: 'associationVo',
+					width: 100,
+				},
+			],
+		},
+		{
+			title: '集团',
+			key: 'groupVo',
+			children: [
+				{
+					title: '实收',
+					dataIndex: 'actualPrice',
+					key: 'groupVo',
+					width: 100,
+				},
+			],
+		},
+		{
+			title: '文旅局',
+			key: 'cultureBureauVo',
+			children: [
+				{
+					title: '实收',
+					dataIndex: 'actualPrice',
+					key: 'cultureBureauVo',
+					width: 100,
+				},
+			],
+		},
+		{
+			title: '一卡通',
+			key: 'yktVo',
+			children: [
+				{
+					title: '实收',
+					dataIndex: 'actualPrice',
+					key: 'yktVo',
+					width: 100,
+				},
+			],
+		},
+		{
+			title: '地接社',
+			key: 'subTravelVo',
+			children: [
+				{
+					title: '实收',
+					dataIndex: 'actualPrice',
+					key: 'subTravelVo',
+					width: 100,
+				},
+				{
+					title: '未消费费用',
+					dataIndex: 'unSettlementPrice',
+					key: 'subTravelVo',
+					width: 100,
+				},
+			],
 		},
 	];
 	return column;
@@ -86,7 +272,7 @@ const tipCancel = () => {
 </script>
 <style scoped lang="scss">
 .batchTransfer {
-	margin: 10px 0;
+	margin: 20px 0;
 	.last-btn {
 		display: flex;
 		justify-content: flex-end;
@@ -98,7 +284,7 @@ const tipCancel = () => {
 		}
 	}
 	.header-total {
-		margin: 5px 0;
+		margin: 10px 0;
 		margin-left: 30px;
 		span {
 			font-size: 16px;
