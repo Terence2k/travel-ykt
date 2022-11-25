cf4b41de75cd5f0d39a9b1d2d250c9e88447bc6c
林佳杰 <linjj@baiyingtech.cn>
2022-11-25 15:19:07 +0800

    fix:bug

--- src/views/settlementManagement/transferManagement/tripList.vue
+++ src/views/settlementManagement/transferManagement/tripList.vue
@@ -7,6 +7,10 @@
 		</div>
 		<CommonTable :dataSource="state.tableData.data" :columns="columns" :scroll="{ x: '100%' }" style="margin-top: 10px">
 			<template #bodyCell="{ column, record }">
+				<template v-if="column.dataIndex === 'ykt'"> {{ getYKT(record, column) }} </template>
+				<template v-if="column.dataIndex === 'bank'"> {{ getBank(record, column) }} </template>
+				<template v-if="column.key === 'ruleMap'"> {{ getRulePrice(record, column) }} </template>
+				<template v-if="column.dataIndex.includes('tualPrice')"> {{ getActualPrice(record, column) }} </template>
 				<template v-if="column.key === 'action'">
 					<div class="action-btns">
 						<a href="javascript:;" @click="lookTrip(record)">查看</a>
@@ -197,6 +201,43 @@ const pageSideChange = (current: number, size: number) => {
 	// onSearch();
 	initList();
 };
+const getYKT = computed(() => (record, column) => {
+	if (record[column.key] && record[column.key]['ykt']) {
+		return record[column.key]['ykt'];
+	}
+	return '';
+});
+const getBank = computed(() => (record, column) => {
+	if (record[column.key] && record[column.key]['bank']) {
+		return record[column.key]['bank'];
+	}
+	return '';
+});
+const getActualPrice = computed(() => (record: any, column: any) => {
+	if (!column.key.includes('List')) {
+		return record[column.key] ? record[column.key]['actualPrice'] : '';
+	} else {
+		if (record[column.key]) {
+			const idx = record[column.key].findIndex((r: any) => r.comprehensiveFeeProductName === column.parentTitle);
+			if (idx !== -1) {
+				return record[column.key][idx][column.dataIndex] || '';
+			}
+		}
+	}
+	return '';
+});
+const getRulePrice = computed(() => (record: any, column: any) => {
+	const ruleColumnKey = column.parent.split('-')[0];
+	for (const key in record[ruleColumnKey]) {
+		if (column.columnParentName === record[ruleColumnKey][key]['comprehensiveFeeProductName']) {
+			for (const subKey in record[ruleColumnKey][key].ruleList) {
+				if (column.title === record[ruleColumnKey][key].ruleList[subKey].ruleName) {
+					return `${record[ruleColumnKey][key].ruleList[subKey].rulePrice}`;
+				}
+			}
+		}
+	}
+});
 </script>
 <style scoped lang="scss">
 .tripList {
