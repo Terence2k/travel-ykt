7ff8f17dcb708d4d9c2fbd7e4600358f2a72f83d
吴泽伟 <wuzw@baiyingtech.cn>
2022-11-25 17:20:18 +0800

    fix: 地接报表bug修复

--- src/views/reportManagement/earthingGeneralReport/index.vue
+++ src/views/reportManagement/earthingGeneralReport/index.vue
@@ -79,7 +79,18 @@ import CommonPagination from '@/components/common/CommonPagination.vue';
 import { settlementOptions } from '@/stores/modules/settlement';
 import type { TableColumnsType } from 'ant-design-vue';
 import api from '@/api';
-import { StateType, DataType, notConsumed, subTravel, fixedColumn, getRulePrice, getActualPrice, getSubTravelVoUnSettlementPrice, getSettlementRule, getSettlementRuleGuide } from './index';
+import {
+	StateType,
+	DataType,
+	notConsumed,
+	subTravel,
+	fixedColumn,
+	getRulePrice,
+	getActualPrice,
+	getSubTravelVoUnSettlementPrice,
+	getSettlementRule,
+	getSettlementRuleGuide,
+} from './index';
 import { log } from 'console';
 
 const options = settlementOptions();
@@ -244,11 +255,11 @@ const columns = computed(() => {
 		}
 	}
 	// 插入未核销费用数据
-	if(column.value.indexOf(notConsumed) == -1){
+	if (column.value.indexOf(notConsumed) == -1) {
 		column.value.push(notConsumed);
 	}
 	// // 插入地接社数据
-	if(column.value.indexOf(subTravel) == -1){
+	if (column.value.indexOf(subTravel) == -1) {
 		column.value.push(subTravel);
 	}
 	// 把所有带有结算规则的数据进行数据整理
@@ -354,7 +365,6 @@ const timeChange = (arr: any) => {
 		state.tableData.param.settlementEndTime = null;
 	}
 };
-
 </script>
 <style scoped lang="less">
 ::v-deep(.ant-table-thead > tr > th, .ant-table-tbody > tr > td, .ant-table tfoot > tr > th, .ant-table tfoot > tr > td) {
