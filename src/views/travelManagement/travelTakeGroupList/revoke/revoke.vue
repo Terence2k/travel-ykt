770f00a4643b7d672c06d7a409e868b8fa34071a
谭健华 <tanjh@baiyingtech.cn>
2022-12-01 17:03:08 +0800

    feat : 【撤销并重提 】详情  枚举

--- src/views/travelManagement/travelTakeGroupList/revoke/revoke.vue
+++ src/views/travelManagement/travelTakeGroupList/revoke/revoke.vue
@@ -75,6 +75,7 @@
 		<CommonTable :columns="tourist" :dataSource="state.touristList" rowKey="oid" :scrollY="false" style="margin-bottom: 40px; padding: 0px">
 			<template #bodyCell="{ column, record, index }">
 				<template v-if="column.key === 'endDate'"> {{ record.startDate }} - {{ record.endDate }} </template>
+				<template v-if="column.key === 'certificateType'"> {{ certificateTypeList[record.certificateType] }} </template>
 			</template>
 		</CommonTable>
 		<p class="top-p">交通信息<span></span></p>
@@ -252,6 +253,10 @@ const guide = [
 		key: 'endDate',
 	},
 ];
+const certificateTypeList = {
+	PASSPORT: '护照',
+	IDENTITY_CARD: '身份证',
+};
 const tourist = [
 	{
 		title: '游客姓名',
