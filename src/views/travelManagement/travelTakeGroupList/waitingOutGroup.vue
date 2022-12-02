9445a6cadd59258db2e539ff77de0df9a19b4c22
骆勇强 <luoyq@baiyingtech.cn>
2022-12-02 09:15:33 +0800

    fix:行程变更

--- src/views/travelManagement/travelTakeGroupList/waitingOutGroup.vue
+++ src/views/travelManagement/travelTakeGroupList/waitingOutGroup.vue
@@ -1,54 +1,36 @@
 <template>
 	<div>
-		<CommonTable :row-selection="{onSelect}" :dataSource="state.tableData" :columns="state.columns" rowKey="oid">
-		<template #button>
-		</template>
-		<template #bodyCell="{ column, text, index, record }">
-      <template v-if="column.key === 'itineraryNo'">
-        <div>
-          <a @click="goToDetail(record)">{{text}}</a>
-        </div>
-      </template>
-			<template v-if="column.key === 'index'">
-				<div>
-						{{(travelStore.takeGroupList.waitingOutGroup.params.pageNo - 1) * (travelStore.takeGroupList.waitingOutGroup.params.pageSize) + (index + 1)}}
-				</div>
-		</template>
-
-		<template v-if="column.key === 'groupTypeStr'">
-				{{text}}
-		</template>
-
-		<template v-if="column.key === 'action'">
-			<div class="action-btns">
-				<a v-if="dateTime > dayjs(record.startDate).unix()" @click="outGroup(record)">手动出团</a>
-				<a @click="goToChange(record)">行程变更</a>
-				<a>查看日志</a>
-			</div>
-		</template>
+		<CommonTable :row-selection="{ onSelect }" :dataSource="state.tableData" :columns="state.columns" rowKey="oid">
+			<template #button> </template>
+			<template #bodyCell="{ column, text, index, record }">
+				<template v-if="column.key === 'itineraryNo'">
+					<div>
+						<a @click="goToDetail(record)">{{ text }}</a>
+					</div>
 				</template>
-		</CommonTable>
-		<BaseModal title="选择可变更项目" v-model="modelValue" :width="500">
-			<div class="model-div">
-				<a-button type="primary" style="width:120px" @click="changeMission">修改出团信息</a-button>
-				<p>包括导游、交通信息、附件内容，散团前均可修改</p>
-			</div>
-			<div class="model-div">
-				<a-button type="primary" style="width:120px" @click="openModifyproduct()">修改预订产品</a-button>
-				<p>包括行程时间、景区、酒店、餐饮等，未核销时可修改</p>
-			</div>
-			<div class="model-div">
-				<a-button type="primary" style="width:120px" @click="addTourist">添加游客</a-button>
-				<p>如有新增游客，可发起一条新行程单的填报</p>
-			</div>
-			<div class="model-div">
-				<a-button type="primary" style="width:120px" @click="revoke">撤销并重提</a-button>
-				<p>如需删除、修改游客，未核销时可撤销原行程单，重新填报</p>
-			</div>
-			<template v-slot:footer>
-				<a-button @click="modelValue=false">取消</a-button>
+				<template v-if="column.key === 'index'">
+					<div>
+						{{
+							(travelStore.takeGroupList.waitingOutGroup.params.pageNo - 1) * travelStore.takeGroupList.waitingOutGroup.params.pageSize + (index + 1)
+						}}
+					</div>
+				</template>
+
+				<template v-if="column.key === 'groupTypeStr'">
+					{{ text }}
+				</template>
+
+				<template v-if="column.key === 'action'">
+					<div class="action-btns">
+						<a v-if="dateTime > dayjs(record.startDate).unix()" @click="outGroup(record)">手动出团</a>
+						<a @click="goToChange(record)">行程变更</a>
+						<a>查看日志</a>
+					</div>
+				</template>
 			</template>
-		</BaseModal>
+		</CommonTable>
+		<ChangeItems v-model="modelValue" :changeParams="state.changeParams"></ChangeItems>
+
 		<CommonPagination
 			:current="travelStore.takeGroupList.waitingOutGroup.params.pageNo"
 			:page-size="travelStore.takeGroupList.waitingOutGroup.params.pageSize"
@@ -59,172 +41,128 @@
 	</div>
 </template>
 <script lang="ts" setup>
-	import CommonTable from '@/components/common/CommonTable.vue';
-	import CommonPagination from '@/components/common/CommonPagination.vue';
-
-	import { useTravelStore } from '@/stores/modules/travelManagement';
-	import { GroupMode, TakeGroupStatus } from '@/enum'
-	import api from '@/api/index';
-  import { message } from 'ant-design-vue';
-	import BaseModal from '@/components/common/BaseModal.vue';
-  import dayjs from 'dayjs';
-
-
-	const travelStore = useTravelStore();
-	const router = useRouter()
-	const modelValue = ref(false)
-  const dateTime = ref(dayjs().unix())
-	const state = reactive({
-		total: computed(() => travelStore.takeGroupList.waitingOutGroup.total),
-		params: {
-				pageNo: 1,
-				pageSize: 10,
-				status: 1
+import CommonTable from '@/components/common/CommonTable.vue';
+import CommonPagination from '@/components/common/CommonPagination.vue';
+import ChangeItems from '@/components/common/changeItems.vue';
+import { useTravelStore } from '@/stores/modules/travelManagement';
+import { GroupMode, TakeGroupStatus } from '@/enum';
+import api from '@/api/index';
+import { message } from 'ant-design-vue';
+import BaseModal from '@/components/common/BaseModal.vue';
+import dayjs from 'dayjs';
+const travelStore = useTravelStore();
+const router = useRouter();
+const modelValue = ref(false);
+const dateTime = ref(dayjs().unix());
+const state = reactive({
+	total: computed(() => travelStore.takeGroupList.waitingOutGroup.total),
+	params: {
+		pageNo: 1,
+		pageSize: 10,
+		status: 1,
+	},
+	changeParams: {} as any,
+	tableData: computed(() => travelStore.takeGroupList.waitingOutGroup.list),
+	columns: [
+		{
+			title: ' 序号 ',
+			key: 'index',
+			width: '80px',
 		},
-		id:'',
-		itineraryNo:'',
-		tableData: computed(() => travelStore.takeGroupList.waitingOutGroup.list),
-		columns: [
-			{
-					title: ' 序号 ',
-					key: 'index',
-					width: '80px'
-			},
-			{
-					title: '行程单号',
-					dataIndex: 'itineraryNo',
-					key: 'itineraryNo',
-			},
-			{
-					title: '线路名称',
-					dataIndex: 'routeName',
-					key: 'routeName',
-			},
-			{
-					title: '组团社',
-					dataIndex: 'travelName',
-					key: 'travelName',
-			},
-			{
-					title: '行程时间',
-					dataIndex: 'time',
-					key: 'time',
-			},
-			{
-					title: '团队类型',
-					dataIndex: 'groupTypeStr',
-					key: 'groupTypeStr',
-			},
-			{
-					title: '带队导游',
-					dataIndex: 'guides',
-					key: 'guides',
-			},
-			{
-					title: '团客人数',
-					dataIndex: 'touristCount',
-					key: 'touristCount',
-			},
-			{
-					title: '操作',
-					fixed: 'right',
-					key: 'action',
-			}
-		]
-	})
-	const onSearch = async () => {
-		travelStore.takeGroupList.waitingOutGroup.params.status = TakeGroupStatus.WaitingOutGroup
-		const res = await travelStore.getTravelList(travelStore.takeGroupList.waitingOutGroup.params);
-		travelStore.setTakeGroupList(res, 'waitingOutGroup')
-	}
-	const onHandleCurrentChange = (e: any) => {
-		travelStore.takeGroupList.waitingOutGroup.params.pageNo = e
-		onSearch()
-  };
-	const pageSideChange = () => {
+		{
+			title: '行程单号',
+			dataIndex: 'itineraryNo',
+			key: 'itineraryNo',
+		},
+		{
+			title: '线路名称',
+			dataIndex: 'routeName',
+			key: 'routeName',
+		},
+		{
+			title: '组团社',
+			dataIndex: 'travelName',
+			key: 'travelName',
+		},
+		{
+			title: '行程时间',
+			dataIndex: 'time',
+			key: 'time',
+		},
+		{
+			title: '团队类型',
+			dataIndex: 'groupTypeStr',
+			key: 'groupTypeStr',
+		},
+		{
+			title: '带队导游',
+			dataIndex: 'guides',
+			key: 'guides',
+		},
+		{
+			title: '团客人数',
+			dataIndex: 'touristCount',
+			key: 'touristCount',
+		},
+		{
+			title: '操作',
+			fixed: 'right',
+			key: 'action',
+		},
+	],
+});
+const onSearch = async () => {
+	travelStore.takeGroupList.waitingOutGroup.params.status = TakeGroupStatus.WaitingOutGroup;
+	const res = await travelStore.getTravelList(travelStore.takeGroupList.waitingOutGroup.params);
+	travelStore.setTakeGroupList(res, 'waitingOutGroup');
+};
+const onHandleCurrentChange = (e: any) => {
+	travelStore.takeGroupList.waitingOutGroup.params.pageNo = e;
+	onSearch();
+};
+const pageSideChange = () => {};
+const outGroup = (row: any) => {
+	console.log('手动出团：', row);
+};
+const goToChange = (row: any) => {
+	state.changeParams.id = row.oid;
+	state.changeParams.itineraryNo = row.itineraryNo;
+	api.travelManagement.checkVerifyByItineraryId(row.itineraryNo).then((res) => {
+		if (res) {
+			modelValue.value = true;
+		} else {
+			message.error('该行程单发生过核销不可变更');
+		}
+	});
+};
+const goToDetail = (row: any) => {
+	router.push({
+		path: '/travel/travel_manage/travel_detail',
+		query: { oid: encodeURIComponent(row.oid) },
+	});
+};
+const onSelect = (record: any, selected: boolean, selectedRows: any[]) => {
+	console.log(record, selected, selectedRows);
+};
+onSearch();
+console.log('dateTime.value:', dateTime.value);
 
-	}
-  const outGroup = (row: any) => {
-    console.log('手动出团：', row);
-    
-  }
-	const goToChange = (row: any) => {
-    state.id = row.oid;
-    state.itineraryNo = row.itineraryNo;
-      api.travelManagement.checkVerifyByItineraryId(row.itineraryNo).then((res) => {
-      if (res) {
-        modelValue.value = true;
-      }else{
-        message.error('该行程单发生过核销不可变更')
-      }
-    });
-  };
-	const changeMission = () => {
-    router.push({
-      path: '/travel/take_group/changetravel',
-      query: {
-        id: state.id,
-        itineraryNo: state.itineraryNo,
-      },
-    });
-  };
-	
-	const openModifyproduct = () => {
-		router.push({
-			path: '/travel/take_group/modify_o_product',
-			query: {
-				oid: state.id,
-			},
-		});
-		
-	}
-	const addTourist = () => {
-		router.push({
-			path: '/travel/take_group/addTourist',
-			query: {
-				id: state.id,
-				itineraryNo: state.itineraryNo,
-			},
-		});
-	};
-	const revoke = () => {
-		router.push({
-			path: '/travel/take_group/revoke',
-			query: {
-				id: state.id,
-				itineraryNo: state.itineraryNo,
-			},
-		});
-	};
-	const goToDetail = (row: any) => {
-		router.push({
-      path: '/travel/travel_manage/travel_detail',
-      query: { oid: encodeURIComponent(row.oid) }
-    });
-	}
-  const onSelect = (record: any, selected: boolean, selectedRows: any[]) => {
-    console.log(record, selected, selectedRows);
-  }
-	onSearch()
-  console.log('dateTime.value:', dateTime.value);
-  
-  watch(
+watch(
 	() => dateTime.value,
 	async (nVal) => {
-    if (nVal) {
-      console.log('dateTime.value:', dateTime.value);
-      
-    }
-	})
+		if (nVal) {
+			console.log('dateTime.value:', dateTime.value);
+		}
+	}
+);
 </script>
 <style lang="less" scoped>
-	.model-div
-	{
-		text-align: center;
-		margin-bottom: 30px;
-	}
-	.model-div>p{
-		color: rgb(225, 225, 225);
-		margin-top: 10px;
-	}
+.model-div {
+	text-align: center;
+	margin-bottom: 30px;
+}
+.model-div > p {
+	color: rgb(225, 225, 225);
+	margin-top: 10px;
+}
 </style>
