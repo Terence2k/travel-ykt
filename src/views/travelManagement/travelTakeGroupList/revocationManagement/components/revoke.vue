9a20ec007590ddc996b8b85a79d0aaa94f7d17b7
谭健华 <tanjh@baiyingtech.cn>
2022-12-02 14:24:57 +0800

    feat : 【撤销并重提 】撤销后重置列表

--- src/views/travelManagement/travelTakeGroupList/revocationManagement/components/revoke.vue
+++ src/views/travelManagement/travelTakeGroupList/revocationManagement/components/revoke.vue
@@ -157,15 +157,7 @@
 			申请整团撤回的 行程单！系统将自动将该结果通知申请发起人。
 		</p>
 		<template v-slot:footer>
-			<a-button
-				@click="
-					() => {
-						reRecokeAuditSuccessVisible = false;
-						modelValue = false;
-					}
-				"
-				>确定</a-button
-			>
+			<a-button @click="finish">确定</a-button>
 		</template>
 	</BaseModal>
 
@@ -317,6 +309,12 @@ const sureAudit = async () => {
 
 	reRecokeAuditSuccessVisible.value = true;
 	reRecokeAuditSureVisible.value = false;
+};
+const emits = defineEmits(['finish']);
+const finish = () => {
+	reRecokeAuditSuccessVisible.value = false;
+	modelValue.value = false;
+	emits('finish');
 };
 
 // 打开弹窗
