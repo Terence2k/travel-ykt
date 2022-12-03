92c5d995617ff339623e9ac25c7c8085f6cd907f
卢智锋 <luzf@baiyingtech.cn>
2022-12-03 15:38:26 +0800

    style:按钮权限指令注释添加

--- src/permission/index.ts
+++ src/permission/index.ts
@@ -1,27 +1,36 @@
+console.log('登录');
 
 let targetMenu: any;
-
-const treeForeach = (tree: any, value: any, tabValue?: any) => {
+// btnValue: 按钮名字,tabValue: tab页名
+const treeForeach = (tree: any, btnValue: any, tabValue?: any) => {
+  // 获取用户有权限菜单
   const userInfo = JSON.parse(<string>localStorage.getItem('userInfo') || '{}');
   return tree.some((item: any) => {
-    if (item.menuName === value) {
+    // 如果菜单名字与按钮名字匹配
+    if (item.menuName === btnValue) {
+      // 通过parentId找出所属父级菜单
       findFatherMenu(userInfo.sysMenuVos, item.parentId);
-      if (targetMenu && !tabValue) return window.location.href.split('/#')[1] === targetMenu.url;
-      if (targetMenu && tabValue) return window.location.href.split('/#')[1] === targetMenu.url
+      // 通过当前路由地址匹配父级菜单是否一致
+      if (targetMenu && !tabValue) return window.location.href.split('/#')[1].indexOf(targetMenu.url) !== -1;
+      if (targetMenu && tabValue) return window.location.href.split('/#')[1].indexOf(targetMenu.url) !== -1
       && 
       targetMenu.childMenuList.some((item: any) => item.menuName === tabValue);
     }
-    if (item.childMenuList?.length) return treeForeach(item.childMenuList, value, tabValue);
+    // 如有子菜单
+    if (item.childMenuList?.length) return treeForeach(item.childMenuList, btnValue, tabValue);
     return false;
   })
 }
 
 const findFatherMenu = (menuList: any, pid: any) => {
+  // 获取用户有权限菜单
   const userInfo = JSON.parse(<string>localStorage.getItem('userInfo') || '{}');
+  // 通过parentId找出所属父级菜单
   const res = menuList.find((item: any) => {
     if (item.childMenuList?.length && item.oid != pid) findFatherMenu(item.childMenuList, pid);
     return item.oid === pid;
   });
+  // 如果菜单类型是tab页,再找其所属父级菜单以找到url
   if (res?.menuType === 2) {
     findFatherMenu(userInfo.sysMenuVos, res.parentId);
   } else if (res) {
@@ -34,6 +43,7 @@ const directives = [
     name: 'permission',
     value: {
       mounted(el: HTMLElement, binding: any) {
+        // 获取用户有权限菜单
         const userInfo = JSON.parse(<string>localStorage.getItem('userInfo') || '{}');
         const type = binding.value || '';
         let status = false;
