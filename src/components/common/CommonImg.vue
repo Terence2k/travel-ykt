797a7b188bb5853b197bedd4c7699a2fa6035b37
luzf <luzf@gzsendi.cn>
2022-12-12 17:13:12 +0800

    feat:添加图片密钥访问

---
+++ src/components/common/CommonImg.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <img v-bind="$attrs" :src="imgUrl">
+    </div>
+</template>
+
+<script setup lang="ts">
+import { awsGetPreSignedUrl } from '@/utils/awsUpload';
+const attrs = useAttrs() as any;
+const imgUrl = ref('');
+
+onMounted(async () => {
+    imgUrl.value = await awsGetPreSignedUrl(attrs.src);
+})
+
+</script>
+
+<style scoped>
+
+</style>
